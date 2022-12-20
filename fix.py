0eb83c88de699a677d4ddc10816de82ba28516db
lievan <0evanli0@gmail.com>
2022-12-20 01:51:06 -0500

    rm py

--- fix.py
+++
@@ -1,6 +0,0 @@
-with open('restaurants1.csv', 'w') as f:
-    with open('restaurants.csv', 'r') as x:
-        lines = x.readlines()
-        for line in lines:
-            l = line.replace(" , ", ", ")
-            f.write(l)
